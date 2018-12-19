25e53d5239df88b712ccc7a75ff08b4f522ac7f6
Kamil Wilczek <kamil.van.wilczek@gmail.com>
2018-12-20 00:26:32 +0100

    Improved the Gnome and Gnome Terminal settings
    - added a lot of settings for the Gnome and Gnome Terminal
    - added a new profile creation method
    - added previously modified install_prerequisites

--- configure_bash_gnome.sh
+++ configure_bash_gnome.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-# Get the default profile ID.
-# But just after changing the name.
-prof_id="$( dconf list /org/gnome/terminal/legacy/profiles:/ )"
+# Gnome Terminal preferences in dconf are stored under this path:
+term_path="/org/gnome/terminal/legacy/profiles:"
 
-# dconf terminal profiles path.
-# prof_id starts with the ':' and ends with '/'.
-prof_path="/org/gnome/terminal/legacy/profiles:/${prof_id}"
+# We need a new UUID for our profile.
+# I do not care if there are different profiles. This is only
+# a run-once setup script that will set me up after a fresh
+# installation. And I do not use more than one Bash profile.
+uuid="$( uuidgen )"
 
-# set the profile name
-dconf write "${prof_path}default-show-menubar"	"false"
-dconf write "${prof_path}use-system-font"				"false"
-dconf write "${prof_path}audible-bell"					"false"
-dconf write "${prof_path}use-theme-colors"			"false"
-dconf write "${prof_path}font"									"'Fira Code Medium 10'"
-dconf write "${prof_path}background-color"			"'rgb(0,0,0)'"
-dconf write "${prof_path}scrollbar-policy"			"'never'"
+# Name the new profile and set as the default one.
+dconf write "${term_path}/:${uuid}/visible-name" "'kamil'"
+dconf write "${term_path}/list" "['${uuid}']"
+dconf write "${term_path}/default" "'${uuid}'"
 
+# Global Gnome Terminal preferences.
 dconf write "/org/gnome/terminal/legacy/default-show-menubar" "false"
+
+# Set favourite preferences in the profile.
+dconf write "${term_path}/:${uuid}/default-show-menubar" "false"
+dconf write "${term_path}/:${uuid}/use-system-font" "false"
+dconf write "${term_path}/:${uuid}/audible-bell" "false"
+dconf write "${term_path}/:${uuid}/use-theme-colors" "false"
+dconf write "${term_path}/:${uuid}/font" "'Fira Code Regular 10'"
+dconf write "${term_path}/:${uuid}/background-color" "'rgb(0,0,0)'"
+dconf write "${term_path}/:${uuid}/scrollbar-policy" "'never'"
+
 dconf write "/org/gnome/desktop/interface/enable-animations" "false"
 dconf write "/org/gnome/desktop/interface/gtk-theme" "'Adwaita-dark'"
 dconf write "/org/gnome/desktop/interface/clock-show-date" "true"
@@ -30,3 +38,6 @@ dconf write "/org/gnome/settings-daemon/plugins/media-keys/terminal" "'<Super>t'
 dconf write "/org/gnome/shell/favorite-apps" "['firefox.desktop', 'org.gnome.Nautilus.desktop', 'libreoffice-writer.desktop']"
 dconf write "/org/gnome/nautilus/preferences/default-folder-viewer" "'list-view'"
 dconf write "/org/gnome/nautilus/list-view/default-zoom-level" "'small'"
+dconf write "/org/gnome/desktop/peripherals/touchpad/natural-scroll" "false"
+dconf write "/org/gnome/settings-daemon/plugins/power/sleep-inactive-ac-type" "'nothing'"
+
