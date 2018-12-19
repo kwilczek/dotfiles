1c669c689e32fed2d545f8b00ca5037397e1b3e4
Kamil Wilczek <kamil.van.wilczek@gmail.com>
2018-12-20 00:24:53 +0100

    Cleanup
    - removed the old role setting Gnome Terminal and Gnome
      preferences
    - renamed the install_ansible script to install_prerequisites
      and added vim and git as the required packages

--- roles/gnome_settings/files/gsettings_ubuntu.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Dock
-gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 36
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
